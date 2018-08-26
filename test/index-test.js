128c5be5eca353fefb764f039546c0a8925d7108
Blaithin Settle <blaithin.settle@yahoo.com>
2018-08-26 07:40:06 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -49,15 +49,17 @@ function destructivelyPrependKitten(Array, name){
     return Array;
   }
 
-  describe('destructivelyRemoveFirstKitten()', function() {
+  /*describe('destructivelyRemoveFirstKitten()', function() {
     it('removes the First kitten from the kittens array', function() {
       destructivelyRemoveFirstKitten()
 
       expect(window.kittens).toEqual(["Otis", "Garfield"])
     })
-  })
+  })*/
 //4 
-
+function destructivelyRemoveFirstKitten(Array){
+  Array.splice(0,1);
+    return Array;}
 
   describe('appendKitten(name)', function() {
     it('appends a kitten to the kittens array and returns a new array, leaving the kittens array unchanged', function() {
